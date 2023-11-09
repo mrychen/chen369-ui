8b2c465a91402ed57b6b5bb57f9e4515a44ef904
chenH <1690599634@qq.com>
2023-11-09 12:52:17 +0800

    xx

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,20 @@
 module.exports = {
     base: '/chen369-ui.github.io/',
     themeConfig: {
+        nav: [{
+                text: 'Home',
+                link: '/'
+
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/mrychen/chen369-ui.github.io'
+            },
+            {
+                text: 'Npm',
+                link: 'https://www.npmjs.com/package/chen369-ui'
+            }
+        ],
         sidebar: [
             '/',
             '/componentsDocs/ChenButton'
